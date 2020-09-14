72be54ca948cd7cbf68b3f77640f4ba7abc40f3d
burcj3622 <burcj3622@wrdsb.ca>
2020-09-14 16:24:05 +0000

    total

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,36 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+   
+   Scanner input = new Scanner (System.in);
     
+    // how many chromebook chargers 
+    System.out.println("How many chromebook chargers?");
+    
+    //ammount of chargers 
+      int A = input.nextInt();
+
+    // ammount of motherboards 
+    System.out.println("How many motherboards?");
+
+    //ammount of motherboards 
+    int B = input.nextInt();
+    
+    //amount mice
+    System.out.println("How many mice?");
+    
+    //number of mice
+    int C = input.nextInt();
+  
+  double total= (A* 34.99 + B * 127.50 + C * 18.00);
+  System.out.println(" Subtotal= " + "$" + total );
+  
+  double tax = ( total * 0.13);
+  
+  System.out.println("Taxes=" + "$" + tax );
+
+    double total1 = (tax+ total);
+  
+  System.out.println("Total=" + "$"+ total1);
   }
 }
